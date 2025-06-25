51d9a46e76d4ee437209891e7a64f3e2c38e5220
jarinox <45308098+jarinox@users.noreply.github.com>
2025-06-25 15:59:25 +0200

    test: CHLR

---
+++ src/test_chlr.cpp
@@ -0,0 +1,56 @@
+#include <routingkit/osm_simple.h>
+#include <routingkit/vector_io.h>
+#include <routingkit/timer.h>
+#include <routingkit/contraction_hierarchy.h>
+#include <routingkit/min_max.h>
+#include <routingkit/inverse_vector.h>
+#include <routingkit/geo_position_to_node.h>
+#include <routingkit/label.h>
+#include <routingkit/io_helper.h>
+
+#include "verify.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace RoutingKit;
+using namespace std;
+
+int main(int argc, char*argv[]){
+    auto graph = simple_load_osm_multi_profile_routing_graph_from_pbf("map.osm.pbf");
+	auto tail = invert_inverse_vector(graph.first_out);
+		
+    auto ch = ContractionHierarchy::build(graph.node_count(), tail, graph.head, graph.travel_time, graph.label);
+    check_contraction_hierarchy_for_errors(ch);
+
+    auto geo_position_to_node = GeoPositionToNode(graph.latitude, graph.longitude);
+    RoutingRequest request = parse_routing_request(argc, argv, geo_position_to_node);
+
+    if(!request.is_valid){
+        cout << "Invalid routing request. Please provide valid coordinates." << endl;
+        return 1;
+    }
+
+    ContractionHierarchyQuery ch_query(ch);
+    ch_query.reset()
+        .add_source(request.from_node)
+        .set_profile(request.profile)
+        .add_target(request.to_node)
+        .run();
+    
+    if(!ch_query.get_used_target()){
+        cout << "No path found from source to target" << endl;
+        return 1;
+    }
+
+    auto path = ch_query.get_node_path();
+
+    cout << request.from_latitude << " " << request.from_longitude << " "
+			<< request.to_latitude << " " << request.to_longitude << endl;
+
+	for(auto x:path)
+		cout << graph.latitude[x] << " " << graph.longitude[x] << " " << human_readable_label(graph.label[x]) << endl;
+    
+}
+
